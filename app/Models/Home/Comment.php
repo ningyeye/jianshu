5ba5d7b3708774d917705306c2dccb244a3a3b87
lining <1366435203@qq.com>
2018-11-26 20:41:42 +0800

    js.sql

--- app/Models/Home/Comment.php
+++ app/Models/Home/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models\Home;
 
 class Comment extends \App\Models\Model
 {
+    // 貌似没用
     public function news()
     {
         return $this->belongsTo('\App\Models\Home\News', 'news_id', 'id');
